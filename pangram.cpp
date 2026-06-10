8ddef13f327e15fdd395b009ce6ccfcc0d6f0336
Shatheeswaran L <shatheeswaranl@gmail.com>
2026-06-10 12:42:24 +0530

    Create pangram.cpp

---
+++ pangram.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    cout<<"Enter a string:";
+    getline(cin,s);
+    int flag;
+    for(char i:s){
+        flag|=(1<<(i-'a'));
+    }
+    if(flag==(1<<26)-1){
+        cout<<"The given string is pangram";
+        return 0;
+    }
+cout<<"The given string is not pangram";
+    return 0;
+}
